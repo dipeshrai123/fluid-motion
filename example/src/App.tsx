e751aa4ccb3ed6210f54a1808370af922aa7f89b
Dipesh Rai <raidipesh78@gmail.com>
2022-09-30 19:49:12 +0545

    feat: basic animation library complete

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,20 +1,27 @@
-import { createAnimatedComponent, AnimatedValue } from "fluid-motion";
+import { createAnimatedComponent, AnimatedValue, timing } from "fluid-motion";
+import { useRef } from "react";
 
 const ADiv = createAnimatedComponent("div");
 
 function App() {
-  const opacity = new AnimatedValue(0);
+  const opacity = useRef(new AnimatedValue(0)).current;
 
   return (
-    <ADiv
-      style={{
-        width: 100,
-        height: 100,
-        backgroundColor: "red",
-        position: "relative",
-        opacity,
-      }}
-    />
+    <>
+      <ADiv
+        style={{
+          width: 100,
+          height: 100,
+          backgroundColor: "red",
+          position: "relative",
+          opacity,
+        }}
+      />
+
+      <button onClick={() => timing(opacity, { toValue: 1 }).start()}>
+        ANIMTE
+      </button>
+    </>
   );
 }
 
--- packages/fluid-motion/src/animated/createAnimatedComponent.tsx
+++ packages/fluid-motion/src/animated/createAnimatedComponent.tsx
@@ -1,22 +1,51 @@
-import React, { useRef, forwardRef, useEffect, useCallback } from "react";
+import React, {
+  useRef,
+  forwardRef,
+  useCallback,
+  MutableRefObject,
+  useState,
+} from "react";
 import { AnimatedProps } from "./AnimatedProps";
-// import { ApplyAnimatedValues } from "./injectable/ApplyAnimatedValues";
+import { ApplyAnimatedValues } from "./injectable/ApplyAnimatedValues";
+
+function useForceUpdate() {
+  const [, f] = useState(false);
+  const forceUpdate = useCallback(() => f((v) => !v), []);
+  return forceUpdate;
+}
 
 export function createAnimatedComponent(Component: any): any {
-  function Wrapper(props: any) {
+  function Wrapper(props: any, _ref: any) {
     const node = useRef<any>(null);
+    const forceUpdate = useForceUpdate();
+    const propsAnimated: MutableRefObject<AnimatedProps | null> = useRef(null);
 
-    // const attachProps = useCallback((props: any) => )
-
-    useEffect(() => {
-      const animatedProps = new AnimatedProps(props, () => false);
-      console.log(animatedProps);
+    const attachProps = useCallback((props: any) => {
+      const oldPropsAnimated = propsAnimated.current;
+      const callback = () => {
+        let didUpdate: false | undefined = false;
+        if (node.current) {
+          didUpdate = ApplyAnimatedValues.current(
+            node.current,
+            propsAnimated.current!.__getAnimatedValue()
+          );
+        }
+        if (!node.current || didUpdate === false) {
+          forceUpdate();
+        }
+      };
+      propsAnimated.current = new AnimatedProps(props, callback);
+      oldPropsAnimated && oldPropsAnimated.__detach();
     }, []);
+
+    attachProps(props);
+
+    const animatedProps = propsAnimated.current?.__getValue();
 
     return (
       <Component
         ref={(childRef: any) => (node.current = childRef)}
-        {...props}
+        {...animatedProps}
       />
     );
   }
